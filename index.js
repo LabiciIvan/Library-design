792b08fe153c0cc6c252607e217b03e97f1321f2
Ioan Labici <ioan.labici@amdaris.com>
2022-09-16 10:44:48 +0300

    changes on result based on search

--- index.js
+++ index.js
@@ -14,15 +14,39 @@ function test2(e) {
 
     console.log('submited value');
     e.target.value = '';
+
+    let bookArray = [' In Search of Lost Time by Marcel Proust', ' Don Quixote by Miguel de Cervantes',
+                     'The Great Gatsby by F. Scott Fitzgerald', ' Moby Dick by Herman Melville', 
+                     'War and Peace by Leo Tolstoy', 'The Odyssey by Homer',
+                      'Madame Bovary by Gustave Flaubert', 'The Divine Comedy by Dante Alighieri',
+                       'The Brothers Karamazov by Fyodor Dostoyevsky',
+                       'The Divine Comedy by Dante Alighieri',
+                       'The Brothers Karamazov by Fyodor Dostoyevsky',
+                       'Madame Bovary by Gustave Flaubert', 'The Divine Comedy by Dante Alighieri',
+                       'The Brothers Karamazov by Fyodor Dostoyevsky',
+                       'The Divine Comedy by Dante Alighieri',
+                       'The Brothers Karamazov by Fyodor Dostoyevsky' ];
+
+    let bodyElement = document.querySelector('.home-body');
+
+    for(let i = 0; i < bookArray.length; ++i) {
+
+        let elementCreated = `<div class='body-result'>
+                                <a href='#' class='body-link'>${bookArray[i]}</a>
+                             </div>`;
+
+        bodyElement.innerHTML += elementCreated;
+    }
+
+    console.log(bodyElement);
 }
 
 
 function showMenu() {
-    console.log('aratam meniul');
-
+ 
+    
     let navLinks = document.querySelectorAll('.nav-link');
-    console.log(navLinks.length)
-
+    
     let tl = gsap.timeline({duration: 0.2});
 
     tl.to(".fa-solid.fa-bars", 
@@ -31,17 +55,26 @@ function showMenu() {
     }, "-=1").to(".navbar", 
     {
         display: 'flex',
-        width: '20%',
+        width: '100%',
         opacity: 1,
         
+    }, "-=1").to(".header-text", 
+    {
+        opacity: 0.5,
+        duration: 0.1
+        
+    }).to(".header-form", 
+    {
+        opacity: 0.5,
+        duration: 0.1
+        
     });
-
+    
     for(let i = 0; i < navLinks.length; ++i) {
         tl.from(navLinks[i], {
             x: 400,
             duration: 0.2
         })
-        console.log(navLinks[i]);
     }
     
 }
@@ -50,6 +83,8 @@ function hideMenu() {
     
     let tl = gsap.timeline({duration: 0.2});
 
+
+
     tl.to(".navbar", 
     {
         display: 'none',
@@ -57,10 +92,21 @@ function hideMenu() {
         opacity: 0,
  
    
-    }, "-=0.2").to(".fa-solid.fa-bars", 
+    }, "-=0.3").to(".fa-solid.fa-bars", 
     {
         opacity: 1,
-    }, "-= 1");
+    }, "-= 1").to(".header-text", 
+    {
+        opacity: 1,
+        duration: 0.1
+        
+    }).to(".header-form", 
+    {
+        opacity: 1,
+        duration: 0.1
+        
+    });
+
 }
 
 function showSearchBar() {
